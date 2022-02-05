f5da678281160034e344ccf15c9008d02f00727d
Blake Beckstrom <dabeck88@gmail.com>
2022-02-05 13:17:47 -0500

    added working function to calc
    rudimentary, fully working calorie calc

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 function calculate() {
+	event.preventDefault();
+
+	let firstName = document.getElementById("first-name").value;
+	let lastName = document.getElementById("last-name").value;
+
 	let genderSelect = document.getElementById("gender-select");
 	let gender = genderSelect.value;
 	let age = document.getElementById("age").value;
-	let height = document.getElementById("height").value;
-	let weight = document.getElementById("weight").value;
-	event.preventDefault();
+	let height = document.getElementById("height").value * 2.54; //cm
+	let weight = document.getElementById("weight").value * 0.453592; //kg
+	let genderNumber;
+
+	if (gender == "male") {
+		genderNumber = 5;
+	} else {
+		genderNumber = -161;
+	}
 	console.log(
-		`gender: ${gender}, age: ${age}, height: ${height}, weight: ${weight}`
+		`first name: ${firstName}, last name ${lastName}, gender: ${gender}, genderNumber: ${genderNumber}, age: ${age}, height: ${height}cm, weight: ${weight}kg`
 	);
+
+	let calculation = Math.round(
+		10 * weight + 6.25 * height - 5 * age + genderNumber
+	);
+
+	document.getElementById("total").innerHTML = `${calculation} calories`;
 }
